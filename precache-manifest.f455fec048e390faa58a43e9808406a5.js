974f64a07c2dd87fec0d58a9b8f89e13557197e5
pandermatt <p.andermatt@me.com>
2020-02-18 15:50:11 +0000

    Deploying to gh-pages from  - 8b7be550f5be290e73a69a0de9ce9558bda13aed 🚀

---
+++ precache-manifest.f455fec048e390faa58a43e9808406a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88144ade9cd36cb4708",
+    "url": "/ba20-kenspace-frontend/css/app.9fe65224.css"
+  },
+  {
+    "revision": "58952e17e203d52f1236c76351760152",
+    "url": "/ba20-kenspace-frontend/img/logo.58952e17.png"
+  },
+  {
+    "revision": "32c63c32ba40c9a88215d7055376797b",
+    "url": "/ba20-kenspace-frontend/index.html"
+  },
+  {
+    "revision": "c88144ade9cd36cb4708",
+    "url": "/ba20-kenspace-frontend/js/app.e8893bcf.js"
+  },
+  {
+    "revision": "c25e99a20ccddbdccd05",
+    "url": "/ba20-kenspace-frontend/js/chunk-vendors.a8957ca7.js"
+  },
+  {
+    "revision": "ee88654b2b460a329a3320d6b0b9e8aa",
+    "url": "/ba20-kenspace-frontend/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ba20-kenspace-frontend/robots.txt"
+  }
+]);
