b90cbd881767564a6365a2c79ebd7ba6d2c38dba
pandermatt <p.andermatt@me.com>
2020-03-23 14:30:49 +0000

    Deploying to gh-pages from  - 8e5d9f79bcd00c21d1a3721b4f82b2eda64e7db6 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ba20-kenspace-frontend/precache-manifest.a69039c62c55d0ff7b31862b527018e5.js"
+  "/ba20-kenspace-frontend/precache-manifest.d88954513cf9c471d4a3ca3dcd98a8d4.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "ba20-kenspace-frontend"});
